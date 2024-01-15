91eb6d36f0e5e744389fc16262fa3cc5694c86a6
[A19] 김진영 <a44428785@gmail.com>
2024-01-15 20:37:06 +0900

    fix: gpt 답변마다 \n이 달라지는 이슈 -> 파싱방법 정규표현식으로 변경

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from flask_cors import CORS
 import random
+import re
 
 load_dotenv()
 
@@ -31,22 +32,18 @@ def firstConversation():
 
   question = request.json['question']
   # QnA_result = { '분류' : '이별', } 
-  #현재 원인 모를 에러 발생
  
   prompt = f'''
   참고사항: `
-  -연애에 대한 내용이 아닐 경우 해결책을 제시하지 말 것.
-  -질문이 연애상담과 관련이 없을 경우 "장난칠거면 가라."라고 짧게 대답할 것.
+  -질문의 카테고리: 연애/이별
+  -질문이 연애와 관련이 없을 경우 "장난칠거면 가라."라고 짧게 대답할 것.
   ''{question}''
 
   참고사항을 참고해서 ''로 감싸진 질문에 대해 답변해줘.
   아래 답변 포맷을 지켜서 그대로 답변해.
   
   <답변 포맷>
-  [주제]: '질문을 3단어 이내로 요약' 
-  [상담]: 
-  '질문에 대한 자연스러운 상담.'
-
+  [주제]: '질문을 한 문장 이내로 요약' \n [상담]: \n  '질문에 대한 답변.'
   '''
 
   # OpenAI 프롬프트 생성 및 실행
@@ -55,7 +52,7 @@ def firstConversation():
     messages=[
       {
           'role': 'system',
-          'content': '너는 연애의 달인으로, 너의 역할은 사람들의 고민에 공감하고 명확한 해결책을 제시하는거야. 공자, 맹자의 말투, 반말을 사용해서 대답해야해. 너의 역할에 대해서는 언급하지마.'
+          'content': '너는 연애의 달인으로, 너의 역할은 사람들의 고민에 공감하고 해결책을 제시하는거야. 공자, 맹자의 말투, 반말을 사용해서 대답해야해. 너의 역할에 대해서는 언급하지마.'
       },
       {
           'role': 'user',
@@ -76,9 +73,18 @@ def firstConversation():
   chatInfo = [chatId, title]
   history.append(chatInfo)
 
+  # 답변에서 '[상담]:' 다음 부분만 파싱
+  # 정규표현식으로 바꾼 이유: \n 개수가 답변에 따라 달라짐, 클라쪽에서 뽑을 때 오류를 없애기 위해서
+  counseling_section = re.search(r'\[상담\]:([\s\S]*)', response_message)
+  if counseling_section:
+    counseling_text = counseling_section.group(1).strip()
+    print('counseling_text:', counseling_text)
+  else: 
+    counseling_text = '뭐라는거야. 알아 듣게 좀 말해보게나.'
+
   # history에 input과 output 저장
-  history.append([question, response_message.split('\n')[2:]])
-  print(history)
+  history.append([question, counseling_text])
+  print('history:', history)
 
   # message 객체의 content 속성을 사용
   return jsonify({"response": history})
