1440b9eea3514f4381dc544332d9c1a2eda94b72
박수진 <wav0120@gmail.com>
2024-01-26 10:45:22 +0900

    Feat: Model API 헤더 인증 임시 추가

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ from flask_cors import CORS
 from services.gpt_service import conversation
 import re, random
 from utils.gpt_util import generate_random_id, parsing_gpt_answer
-from utils.model_util import parse_response_by_label
+from utils.model_util import parse_response_by_label, validate_by_base64
 import tensorflow as tf
 from flask import g
 from transformers import TFBertModel, pipeline , BertTokenizer, TFBertForSequenceClassification
+import base64
 
 app = Flask(__name__)
 CORS(app)
 
 
 def load_model():
-  print('model을 불러오고 있습니다..')
   global classifier
   model_name = "guru_model.h5"
+  print(f"모델을 불러오고 있습니다..")
+
   tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
   classifier = pipeline("text-classification", model=model_name, tokenizer=tokenizer)
   
@@ -87,9 +89,24 @@ def additionalConversation(chatId):
 def textAnalysis():
   global classifier
 
+  # 헤더에서 인증 정보를 받아옴
+  if request.headers.get('Authorization'):
+    auth = request.headers.get('Authorization')
+  
+    username = auth.split(' ')[1].split(':')[0]
+    password = auth.split(' ')[1].split(':')[1]
+
+    if not validate_by_base64(username, password):
+      print("불일치!!")
+      return 'Unauthroization!', 401
+    else:
+      print("인증 성공")
+  else:
+    print("warn:: 인증 헤더가 포함되지 않았습니다!")
+
   # 전역 변수에 저장된 classifier가 없으면 새로 정의함
   if not classifier:
-      print('model을 다시 불러옵니다..')
+      print(f"model을 다시 불러옵니다..")
       load_model()
 
   content = request.json['content']
